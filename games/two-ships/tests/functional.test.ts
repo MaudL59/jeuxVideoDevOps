1b7fa862197ea7b8e273f6b9337eaf5c462b5b76
mina-barry <aminata.barry@epitech.eu>
2026-04-15 14:32:00 +0200

    test de fonctional et unit

--- games/two-ships/tests/functional.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest';
-
-describe('Tests de mon jeu', () => {
-  // On va mettre le test ici
-  
-  it('devrait calculer 4', () => {
-  expect(2 + 2).toBe(4);
-});
-});
